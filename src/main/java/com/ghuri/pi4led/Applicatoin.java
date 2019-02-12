6241cf6e292dc22e5a4fe1f89056113cfd6ddc1d
iqbalfoy <47431271+iqbalfoy@users.noreply.github.com>
2019-02-12 11:02:11 +0600

    Add files via upload

---
+++ src/main/java/com/ghuri/pi4led/Applicatoin.java
@@ -0,0 +1,12 @@
+package com.ghuri.pi4led;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applicatoin {
+    public static void main(String[] args){
+        SpringApplication.run(Applicatoin.class,args);
+
+    }
+}
